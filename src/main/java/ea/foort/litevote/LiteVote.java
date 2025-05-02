a39c2b92aea5eea27ed67feb56a67ef0f43d314e
Foortiks <aleks11012011n7@inbox.ru>
2025-05-02 21:12:16 +0500

    Update LiteVote.java

--- src/main/java/ea/foort/litevote/LiteVote.java
+++ src/main/java/ea/foort/litevote/LiteVote.java
@@ -6,18 +6,21 @@ import ea.foort.litevote.commands.VoteCommand;
 import ea.foort.litevote.config.ConfigManager;
 import ea.foort.litevote.managers.VoteManager;
 import ea.foort.litevote.menus.VoteMenu;
+import ea.foort.litevote.utils.Timer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LiteVote extends JavaPlugin {
     private static LiteVote instance;
     private ConfigManager configManager;
     private VoteManager voteManager;
+    private Timer timer;
 
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
         this.configManager = new ConfigManager(this);
         this.voteManager = new VoteManager(this);
+        this.timer = new Timer(this, voteManager);
         getServer().getPluginManager().registerEvents(new VoteMenu(voteManager), this);
         getCommand("vote").setExecutor(new VoteCommand(voteManager));
         getCommand("startvote").setExecutor(new StartVoteCommand(voteManager));
